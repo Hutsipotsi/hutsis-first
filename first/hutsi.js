caef4ca01530e589ad273737146c62e41699f271
Hutsipotsi <93660933+Hutsipotsi@users.noreply.github.com>
2021-11-04 22:14:54 +0200

    main

--- first/hutsi.js
+++ first/hutsi.js
@@ -1,4 +1,13 @@
 function tell() {
     let name = document.getElementById('name').value;
-    return document.getElementById('toldName').innerHTML = "Welcome to my first GitHub site " + name 
+
+    document.getElementById('name').innerHTML = "";
+    document.getElementById('tellName').innerHTML = "";
+    document.getElementById('nameError').innerHTML = "";
+
+    if(name === "") {
+        document.getElementById('nameError').innerHTML = "Write your name";
+        return;
+    }
+    document.getElementById('tellName').innerHTML = "Welcome to my first GitHub site " + name + "!"
 }
